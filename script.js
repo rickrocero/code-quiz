a7c28d86772d96f872ac5897bed708dc872c9132
ricroc <rickrocero@gmail.com>
2021-03-26 17:17:59 -0700

    basic website set up and updated js file with pseudocode

---
+++ script.js
@@ -0,0 +1,20 @@
+//1. Take the coding quiz
+    // clicking start quiz button presents user with first question
+    // track click target
+    // correct answers are tracked
+    // Next question is presented immediately after user picks an answer
+    // User is notified of correct or wrong answer
+
+//2. Countdown Timer
+    // give user 75 seconds to take quiz
+    // display countdown on screen
+    // wrong answers shave 10 seconds off timer
+    // if time runs out, loss
+
+//3. Save wins/losses
+    // after quiz, update final score
+    // save to local storage
+    // user submits name and highscore
+    // save to local storage
+    // user is taken to highscore page
+    // user is presented with highscores list and buttons to retake quiz or clear highscore list.
